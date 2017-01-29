fa958b40b5dab2f2fd3330e81d428a3578d25e6f
Alexandr Matskevich <alxmats@gmail.com>
2017-01-29 22:17:46 +0300

    guessing-game/feature: all methods were implemented

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -3,17 +3,26 @@ class GuessingGame {
 
     setRange(min, max) {
 
+    	this.left = min;
+    	this.right = max;
+
     }
 
     guess() {
+
+    	return (Math.round((this.right+this.left)/2));
 
     }
 
     lower() {
 
+    	return this.setRange (this.left, this.guess());
+
     }
 
     greater() {
+
+    	return this.setRange (this.guess(), this.right)
 
     }
 }
